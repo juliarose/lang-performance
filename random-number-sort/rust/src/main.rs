8813928cff686528a36cee68d67e686abc53762e
juliarose <julialunarose@gmail.com>
2022-03-27 06:11:16 -0400

    changed rust example to be more simple and node example to use bitwise NOT for rounding

--- random-number-sort/rust/src/main.rs
+++ random-number-sort/rust/src/main.rs
@@ -1,18 +1,21 @@
 use std::time::Instant;
 use rand::Rng;
 use thousands::Separable;
-use itertools::sorted;
 
 fn main() {
     let now = Instant::now();
     
     let mut rng = rand::thread_rng();
-    let _values = sorted(
-        (0..1000000)
-            .map(|_| rng.gen_range(0..1000000))
-    )
-            .map(|a| a.separate_with_commas())
-            .collect::<Vec<String>>();
+    let mut values = (0..1000000)
+        .map(|_| rng.gen_range(0..1000000))
+        .collect::<Vec<u32>>();
+    
+    values.sort();
+    
+    let _values = values
+        .iter()
+        .map(|a| a.separate_with_commas())
+        .collect::<Vec<String>>();
     let duration = now.elapsed();
     
     println!("{}ms", duration.as_micros() as f32 / 1000.0);
